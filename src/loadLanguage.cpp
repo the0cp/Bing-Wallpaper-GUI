886a90731eb2906260fed6e403e0d9f37de0d49d
Theodore Cooper <63190431+theodorecooper@users.noreply.github.com>
2021-06-30 14:59:56 +0800

    Add files via upload

--- src/loadLanguage.cpp
+++ src/loadLanguage.cpp
@@ -1,4 +1,26 @@
-﻿#include "include/bingbg.h"
+﻿/***************************************************************************
+ * Author: Theodore Cooper                                                 *
+ * License: GNU General Public License                                     *
+ *                                                                         *
+ * Bing-Backgrounds-Getter-v1.2.2                                          *
+ *                                                                         *
+ * Copyright (C) 2021 Theodore Cooper <ccooperr2005@gmail.com>             *
+ *                                                                         *
+ * This program is free software: you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation, either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ ***************************************************************************/
+
+#include "include/bingbg.h"
 #include "ui_bingbg.h"
 
 void BingBG::loadLanguage(int LANGINDEX)
